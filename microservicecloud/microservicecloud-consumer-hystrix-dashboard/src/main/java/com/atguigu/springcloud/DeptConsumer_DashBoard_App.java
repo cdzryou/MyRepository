090fa5408a07c3298732df4b3b8e9ede77fc2d71
zryou_company <cdzryou@163.com>
2019-11-15 15:00:56 +0800

    create microservicecloud hystrix_dashboard

---
+++ microservicecloud/microservicecloud-consumer-hystrix-dashboard/src/main/java/com/atguigu/springcloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author 游正荣
+ * @create 2019-11-15 10:39
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DeptConsumer_DashBoard_App {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_DashBoard_App.class,args);
+    }
+}
